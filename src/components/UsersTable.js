cfd9393b582dd21c4b2230ba8610accec3352e99
DivineFavour <edehdivine042@gmail.com>
2022-05-27 14:36:58 +0100

    fixed tables

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Axios from 'axios'
 import UserDetails from './UserDetails';
+// import "../App.css"
 
 const UsersTable = ({ users, loading, error }) => {
     const [posts, setPosts] = useState();
@@ -32,39 +33,41 @@ const UsersTable = ({ users, loading, error }) => {
 
                         return <div className='hover:cursor-pointer' key={item?.id} onClick={() => handleClicked(item?.id)}>
                             <div className='flex text-[#505050] shadow-md rounded-md px-3 mb-5'>
-                                <div className='grid grid-cols-10 my-4 content-start'>
-                                    <div className='flex col-span-1'>
+                                <table className="w-full">
+                                <thead>
+                                <tr>
+                                    <td className="flex mb-4 w-10">
                                         <input className='mr-3 mt-[5px] hover:cursor-pointer' type="checkbox" />
                                         <p className='text-[#A8A8A8]'>{item?.id}</p>
-                                    </div>
-                                    <div className='col-span-1'>
+                                    </td>
+                                    <td>
                                         <p>{item?.gender === "male" ? "Mr" : "Mrs"}</p>
-                                    </div>
-                                    <div className='col-span-4'>
-                                        <div className='flex'>
-                                            <img className='rounded-full mr-3 w-10' src={item?.image} alt="profile_image" />
-                                            <div className='block'>
-                                                <div className='flex text-[#9991E5]'>
-                                                    <p className='mr-1'>{item?.firstName}</p>
-                                                    <p>{item?.lastName}</p>
-                                                </div>
-
-                                                <p className='text-[#A8A8A8] text-sm'>@{item?.username}</p>
+                                    </td>
+                                    <td className="flex">
+                                        <img className='rounded-full mr-3 w-10' src={item?.image} alt="profile_image" />
+                                        <div className='block'>
+                                            <div className='flex text-[#9991E5]'>
+                                                <p className='mr-1'>{item?.firstName}</p>
+                                                <p>{item?.lastName}</p>
                                             </div>
+
+                                            <p className='text-[#A8A8A8] text-sm'>@{item?.username}</p>
                                         </div>
-                                    </div>
-                                    <div className='col-span-1'>
+                                    </td>
+                                    <td>
                                         <p>{item?.id}</p>
-                                    </div>
-                                    <div className='col-span-2'>
+                                    </td>
+                                    <td>
                                         <p>{item?.phone}</p>
-                                    </div>
-                                </div>
-                                <div className='py-5 hover:cursor-pointer col-span-1'>
-                                    <div className='w-1 h-1 bg-[#352E4D] mb-1 rounded-full'></div>
-                                    <div className='w-1 h-1 bg-[#352E4D] mb-1 rounded-full'></div>
-                                    <div className='w-1 h-1 bg-[#352E4D] mb-1 rounded-full'></div>
-                                </div>
+                                    </td>
+                                    <td className="py-5 hover:cursor-pointer">
+                                        <div className='w-1 h-1 bg-[#352E4D] mb-1 rounded-full'></div>
+                                        <div className='w-1 h-1 bg-[#352E4D] mb-1 rounded-full'></div>
+                                        <div className='w-1 h-1 bg-[#352E4D] mb-1 rounded-full'></div>
+                                    </td>
+                                </tr>
+                                </thead>
+                                </table>
                             </div>
                         </div>
 
